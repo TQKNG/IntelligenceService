b4e003ce17f3eddd3cb6662dc459a78cbf28f4c0
Khanh Ng <83807651+TQKNG@users.noreply.github.com>
2024-06-25 12:00:47 -0400

    delete prompt service

--- app/services/prompt_service.py
+++
@@ -1,9 +0,0 @@
-class PromptService:
-    def __init__(self, prompt):
-        self.prompt = prompt
-
-    def get_prompt(self):
-        return self.prompt
-
-    def set_prompt(self, prompt):
-        self.prompt = prompt
