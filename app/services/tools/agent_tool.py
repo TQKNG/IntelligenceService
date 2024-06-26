11c39df86b8c10a6c7ae619af6379745f9e3dfc9
Khanh Ng <83807651+TQKNG@users.noreply.github.com>
2024-06-26 15:14:24 -0400

    Factorize the tools for agent

---
+++ app/services/tools/agent_tool.py
@@ -0,0 +1,25 @@
+# Import abstract syntax grammar
+import ast
+import re
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+### Helper functions ###
+# Flattening and filtering truthy values:
+def query_as_list(db,query):
+    res = db.run(query)
+    res = [el for sub in ast.literal_eval(res) for el in sub if el]
+    res = [re.sub(r"\b\d+\b", "", string).strip() for string in res]
+    return list(set(res))
+
+# Pandas tool
+def generate_dataframe(sql, db):
+    df = pd.read_sql(sql, db)
+    return df
+
+# interpolate_data
+def interpolate_data(df):
+    df = df.interpolate(method='linear', limit_direction='forward', axis=0)
+    return df
+
